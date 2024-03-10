e933cb4602f382c678ab38d7ca80d022ee0eeb28
fabiobento <fabio.obento@gmail.com>
2024-03-10 21:35:47 +0000

    lab

--- 00_course_folder/nn_adv/class_01/11 - Atividade avaliativa - Redes Neurais/public_tests.py
+++ 00_course_folder/nn_adv/class_01/11 - Atividade avaliativa - Redes Neurais/public_tests.py
@@ -1,103 +1,131 @@
-# UNIT TESTS
+# Testes das Unidades
 from tensorflow.keras.activations import sigmoid as tf_keras_sigmoid
 from tensorflow.keras.layers import Dense
 
 import numpy as np
 
+
 def test_c1(target):
-    assert len(target.layers) == 3, \
-        f"Wrong number of layers. Expected 3 but got {len(target.layers)}"
-    assert target.input.shape.as_list() == [None, 400], \
-        f"Wrong input shape. Expected [None,  400] but got {target.input.shape.as_list()}"
+    assert (
+        len(target.layers) == 3
+    ), f"Número errado de camadas. Esperava 3, mas recebi {len(target.layers)}"
+    assert target.input.shape.as_list() == [
+        None,
+        400,
+    ], f"Forma de entrada incorreta. Esperava [None, 400], mas obtive {target.input.shape.as_list()}"
     i = 0
-    expected = [[Dense, [None, 25], tf_keras_sigmoid],
-                [Dense, [None, 15], tf_keras_sigmoid],
-                [Dense, [None, 1], tf_keras_sigmoid]]
+    expected = [
+        [Dense, [None, 25], tf_keras_sigmoid],
+        [Dense, [None, 15], tf_keras_sigmoid],
+        [Dense, [None, 1], tf_keras_sigmoid],
+    ]
 
     for layer in target.layers:
-        assert type(layer) == expected[i][0], \
-            f"Wrong type in layer {i}. Expected {expected[i][0]} but got {type(layer)}"
-        assert layer.output.shape.as_list() == expected[i][1], \
-            f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
-        assert layer.activation == expected[i][2], \
-            f"Wrong activation in layer {i}. Expected {expected[i][2]} but got {layer.activation}"
+        assert (
+            type(layer) == expected[i][0]
+        ), f"Tipo errado na camada {i}. Esperado {expected[i][0]} mas obtive {type(layer)}"
+        assert (
+            layer.output.shape.as_list() == expected[i][1]
+        ), f"Número incorreto de unidades na camada {i}. Esperado {expected[i][1]} mas obtive {layer.output.shape.as_list()}"
+        assert (
+            layer.activation == expected[i][2]
+        ), f"Ativação incorreta na camada {i}. Esperado {expected[i][2]} mas obtive {layer.activation}"
         i = i + 1
 
-    print("\033[92mAll tests passed!")
-    
+    print("\033[92mTodos os testes foram aprovados!")
+
+
 def test_c2(target):
-    
+
     def linear(a):
         return a
-    
+
     def linear_times3(a):
         return a * 3
-    
-    x_tst = np.array([1., 2., 3., 4.])  # (1 examples, 3 features)
-    W_tst = np.array([[1., 2.], [1., 2.], [1., 2.], [1., 2.]]) # (3 input features, 2 output features)
-    b_tst = np.array([0., 0.])  # (2 features)
-    
+
+    x_tst = np.array([1.0, 2.0, 3.0, 4.0])  # (1 examples, 3 features)
+    W_tst = np.array(
+        [[1.0, 2.0], [1.0, 2.0], [1.0, 2.0], [1.0, 2.0]]
+    )  # (3 input features, 2 output features)
+    b_tst = np.array([0.0, 0.0])  # (2 features)
+
     A_tst = target(x_tst, W_tst, b_tst, linear)
     assert A_tst.shape[0] == len(b_tst)
-    assert np.allclose(A_tst, [10., 20.]), \
-        "Wrong output. Check the dot product"
-    
-    b_tst = np.array([3., 5.])  # (2 features)
-    
+    assert np.allclose(
+        A_tst, [10.0, 20.0]
+    ), "Saída incorreta. Verifique o produto escalar"
+
+    b_tst = np.array([3.0, 5.0])  # (2 features)
+
     A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert np.allclose(A_tst, [13., 25.]), \
-        "Wrong output. Check the bias term in the formula"
-    
+    assert np.allclose(
+        A_tst, [13.0, 25.0]
+    ), "Saída incorreta. Verifique o termo de polarização na fórmula"
+
     A_tst = target(x_tst, W_tst, b_tst, linear_times3)
-    assert np.allclose(A_tst, [39., 75.]), \
-        "Wrong output. Did you apply the activation function at the end?"
-    
-    print("\033[92mAll tests passed!")  
-    
+    assert np.allclose(
+        A_tst, [39.0, 75.0]
+    ), "Saída incorreta. Você aplicou a função de ativação no final?"
+
+    print("\033[92mTodos os testes foram aprovados!")
+
+
 def test_c3(target):
-    
+
     def linear(a):
         return a
-    
+
     def linear_times3(a):
         return a * 3
-    
-    x_tst = np.array([1., 2., 3., 4.])  # (1 examples, 3 features)
-    W_tst = np.array([[1., 2.], [1., 2.], [1., 2.], [1., 2.]]) # (3 input features, 2 output features)
-    b_tst = np.array([0., 0.])  # (2 features)
-    
+
+    x_tst = np.array([1.0, 2.0, 3.0, 4.0])  # (1 examples, 3 features)
+    W_tst = np.array(
+        [[1.0, 2.0], [1.0, 2.0], [1.0, 2.0], [1.0, 2.0]]
+    )  # (3 input features, 2 output features)
+    b_tst = np.array([0.0, 0.0])  # (2 features)
+
     A_tst = target(x_tst, W_tst, b_tst, linear)
     assert A_tst.shape[0] == len(b_tst)
-    assert np.allclose(A_tst, [10., 20.]), \
-        "Wrong output. Check the dot product"
-    
-    b_tst = np.array([3., 5.])  # (2 features)
-    
+    assert np.allclose(
+        A_tst, [10.0, 20.0]
+    ), "Saída incorreta. Verifique o produto escalar"
+
+    b_tst = np.array([3.0, 5.0])  # (2 features)
+
     A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert np.allclose(A_tst, [13., 25.]), \
-        "Wrong output. Check the bias term in the formula"
-    
+    assert np.allclose(
+        A_tst, [13.0, 25.0]
+    ), "Saída incorreta. Verifique o termo de polarização na fórmula"
+
     A_tst = target(x_tst, W_tst, b_tst, linear_times3)
-    assert np.allclose(A_tst, [39., 75.]), \
-        "Wrong output. Did you apply the activation function at the end?"
-    
-    x_tst = np.array([[1., 2., 3., 4.], [5., 6., 7., 8.]])  # (2 examples, 4 features)
-    W_tst = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [10., 11., 12]]) # (3 input features, 2 output features)
-    b_tst = np.array([0., 0., 0.])  # (2 features)
-    
+    assert np.allclose(
+        A_tst, [39.0, 75.0]
+    ), "Saída incorreta. Você aplicou a função de ativação no final?"
+
+    x_tst = np.array(
+        [[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]]
+    )  # (2 examples, 4 features)
+    W_tst = np.array(
+        [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [10.0, 11.0, 12]]
+    )  # (3 input features, 2 output features)
+    b_tst = np.array([0.0, 0.0, 0.0])  # (2 features)
+
     A_tst = target(x_tst, W_tst, b_tst, linear)
     assert A_tst.shape == (2, 3)
-    assert np.allclose(A_tst, [[ 70.,  80.,  90.], [158., 184., 210.]]), \
-        "Wrong output. Check the dot product"
-    
-    b_tst = np.array([3., 5., 6])  # (3 features)
-    
+    assert np.allclose(
+        A_tst, [[70.0, 80.0, 90.0], [158.0, 184.0, 210.0]]
+    ), "Saída incorreta. Verifique o produto escalar"
+
+    b_tst = np.array([3.0, 5.0, 6])  # (3 features)
+
     A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert np.allclose(A_tst, [[ 73.,  85.,  96.], [161., 189., 216.]]), \
-        "Wrong output. Check the bias term in the formula"
-    
+    assert np.allclose(
+        A_tst, [[73.0, 85.0, 96.0], [161.0, 189.0, 216.0]]
+    ), "Saída incorreta. Verifique o termo de polarização na fórmula"
+
     A_tst = target(x_tst, W_tst, b_tst, linear_times3)
-    assert np.allclose(A_tst, [[ 219.,  255.,  288.], [483., 567., 648.]]), \
-        "Wrong output. Did you apply the activation function at the end?"
-    
-    print("\033[92mAll tests passed!")  
+    assert np.allclose(
+        A_tst, [[219.0, 255.0, 288.0], [483.0, 567.0, 648.0]]
+    ), "Saída incorreta. Você aplicou a função de ativação no final?"
+
+    print("\033[92mTodos os testes foram aprovados!")
