62aa96ae392a6c639acf454634194041204dc644
fabiobento <fabio.obento@gmail.com>
2024-03-21 12:17:41 +0000

    perda logística

--- 00_course_folder/ml_intro/class_03/Laboratórios/plt_logistic_loss.py
+++ 00_course_folder/ml_intro/class_03/Laboratórios/plt_logistic_loss.py
@@ -1,13 +1,14 @@
 """----------------------------------------------------------------
- logistic_loss plotting routines and support
+Rotinas e suporte de plotagem de Logistics_loss
 """
 
 from matplotlib import cm
 from lab_utils_common import sigmoid, dlblue, dlorange, np, plt, compute_cost_matrix
 
+
 def compute_cost_logistic_sq_err(X, y, w, b):
     """
-    compute sq error cost on logicist data (for negative example only, not used in practice)
+    calcular o custo do erro quadrado em dados lógicos (apenas para exemplo negativo, não usado na prática)
     Args:
       X (ndarray): Shape (m,n) matrix of examples with multiple features
       w (ndarray): Shape (n)   parameters for prediction
@@ -18,75 +19,96 @@ def compute_cost_logistic_sq_err(X, y, w, b):
     m = X.shape[0]
     cost = 0.0
     for i in range(m):
-        z_i = np.dot(X[i],w) + b
-        f_wb_i = sigmoid(z_i)                 #add sigmoid to normal sq error cost for linear regression
-        cost = cost + (f_wb_i - y[i])**2
+        z_i = np.dot(X[i], w) + b
+        f_wb_i = sigmoid(
+            z_i
+        )  # adiciona sigmóide ao custo do erro quadrado normal para regressão linear
+        cost = cost + (f_wb_i - y[i]) ** 2
     cost = cost / (2 * m)
     return np.squeeze(cost)
 
-def plt_logistic_squared_error(X,y):
-    """ plots logistic squared error for demonstration """
-    wx, by = np.meshgrid(np.linspace(-6,12,50),
-                         np.linspace(10, -20, 40))
+
+def plt_logistic_squared_error(X, y):
+    """traça o erro quadrático logístico para demonstração"""
+    wx, by = np.meshgrid(np.linspace(-6, 12, 50), np.linspace(10, -20, 40))
     points = np.c_[wx.ravel(), by.ravel()]
     cost = np.zeros(points.shape[0])
 
     for i in range(points.shape[0]):
-        w,b = points[i]
-        cost[i] = compute_cost_logistic_sq_err(X.reshape(-1,1), y, w, b)
+        w, b = points[i]
+        cost[i] = compute_cost_logistic_sq_err(X.reshape(-1, 1), y, w, b)
     cost = cost.reshape(wx.shape)
 
     fig = plt.figure()
     fig.canvas.toolbar_visible = False
     fig.canvas.header_visible = False
     fig.canvas.footer_visible = False
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
-    ax.plot_surface(wx, by, cost, alpha=0.6,cmap=cm.jet,)
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+    ax.plot_surface(
+        wx,
+        by,
+        cost,
+        alpha=0.6,
+        cmap=cm.jet,
+    )
 
-    ax.set_xlabel('w', fontsize=16)
-    ax.set_ylabel('b', fontsize=16)
+    ax.set_xlabel("w", fontsize=16)
+    ax.set_ylabel("b", fontsize=16)
     ax.set_zlabel("Cost", rotation=90, fontsize=16)
-    ax.set_title('"Logistic" Squared Error Cost vs (w, b)')
+    ax.set_title('Custo de erro quadrático "logístico" vs (w, b)')
     ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 
 
-def plt_logistic_cost(X,y):
-    """ plots logistic cost """
-    wx, by = np.meshgrid(np.linspace(-6,12,50),
-                         np.linspace(0, -20, 40))
+def plt_logistic_cost(X, y):
+    """traça o custo logístico"""
+    wx, by = np.meshgrid(np.linspace(-6, 12, 50), np.linspace(0, -20, 40))
     points = np.c_[wx.ravel(), by.ravel()]
-    cost = np.zeros(points.shape[0],dtype=np.longdouble)
+    cost = np.zeros(points.shape[0], dtype=np.longdouble)
 
     for i in range(points.shape[0]):
-        w,b = points[i]
-        cost[i] = compute_cost_matrix(X.reshape(-1,1), y, w, b, logistic=True, safe=True)
+        w, b = points[i]
+        cost[i] = compute_cost_matrix(
+            X.reshape(-1, 1), y, w, b, logistic=True, safe=True
+        )
     cost = cost.reshape(wx.shape)
 
-    fig = plt.figure(figsize=(9,5))
+    fig = plt.figure(figsize=(9, 5))
     fig.canvas.toolbar_visible = False
     fig.canvas.header_visible = False
     fig.canvas.footer_visible = False
-    ax = fig.add_subplot(1, 2, 1, projection='3d')
-    ax.plot_surface(wx, by, cost, alpha=0.6,cmap=cm.jet,)
+    ax = fig.add_subplot(1, 2, 1, projection="3d")
+    ax.plot_surface(
+        wx,
+        by,
+        cost,
+        alpha=0.6,
+        cmap=cm.jet,
+    )
 
-    ax.set_xlabel('w', fontsize=16)
-    ax.set_ylabel('b', fontsize=16)
-    ax.set_zlabel("Cost", rotation=90, fontsize=16)
-    ax.set_title('Logistic Cost vs (w, b)')
+    ax.set_xlabel("w", fontsize=16)
+    ax.set_ylabel("b", fontsize=16)
+    ax.set_zlabel("Custo", rotation=90, fontsize=16)
+    ax.set_title("Custo logístico vs (w, b)")
     ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 
-    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax = fig.add_subplot(1, 2, 2, projection="3d")
 
-    ax.plot_surface(wx, by, np.log(cost), alpha=0.6,cmap=cm.jet,)
+    ax.plot_surface(
+        wx,
+        by,
+        np.log(cost),
+        alpha=0.6,
+        cmap=cm.jet,
+    )
 
-    ax.set_xlabel('w', fontsize=16)
-    ax.set_ylabel('b', fontsize=16)
-    ax.set_zlabel('\nlog(Cost)', fontsize=16)
-    ax.set_title('log(Logistic Cost) vs (w, b)')
+    ax.set_xlabel("w", fontsize=16)
+    ax.set_ylabel("b", fontsize=16)
+    ax.set_zlabel("\nlog(Cost)", fontsize=16)
+    ax.set_title("log(Custo Logístico) vs (w, b)")
     ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
@@ -96,96 +118,133 @@ def plt_logistic_cost(X,y):
 
 
 def soup_bowl():
-    """ creates 3D quadratic error surface """
-    #Create figure and plot with a 3D projection
-    fig = plt.figure(figsize=(4,4))
+    """cria superfície de erro quadrática 3D"""
+    # Crie figura e plote com projeção 3D
+    fig = plt.figure(figsize=(4, 4))
     fig.canvas.toolbar_visible = False
     fig.canvas.header_visible = False
     fig.canvas.footer_visible = False
 
-    #Plot configuration
-    ax = fig.add_subplot(111, projection='3d')
+    # Plotar a configuração
+    ax = fig.add_subplot(111, projection="3d")
     ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.zaxis.set_rotate_label(False)
     ax.view_init(15, -120)
 
-    #Useful linearspaces to give values to the parameters w and b
+    # Espaços lineares úteis para fornecer valores aos parâmetros w e b
     w = np.linspace(-20, 20, 100)
     b = np.linspace(-20, 20, 100)
 
-    #Get the z value for a bowl-shaped cost function
-    z=np.zeros((len(w), len(b)))
-    j=0
+    # Obtenha o valor z para uma função de custo em forma de tigela
+    z = np.zeros((len(w), len(b)))
+    j = 0
     for x in w:
-        i=0
+        i = 0
         for y in b:
-            z[i,j] = x**2 + y**2
-            i+=1
-        j+=1
+            z[i, j] = x**2 + y**2
+            i += 1
+        j += 1
 
-    #Meshgrid used for plotting 3D functions
+    # Meshgrid usado para plotar funções 3D
     W, B = np.meshgrid(w, b)
 
-    #Create the 3D surface plot of the bowl-shaped cost function
-    ax.plot_surface(W, B, z, cmap = "Spectral_r", alpha=0.7, antialiased=False)
-    ax.plot_wireframe(W, B, z, color='k', alpha=0.1)
+    # Crie o gráfico de superfície 3D da função de custo em forma de tigela
+    ax.plot_surface(W, B, z, cmap="Spectral_r", alpha=0.7, antialiased=False)
+    ax.plot_wireframe(W, B, z, color="k", alpha=0.1)
     ax.set_xlabel("$w$")
     ax.set_ylabel("$b$")
-    ax.set_zlabel("Cost", rotation=90)
-    ax.set_title("Squared Error Cost used in Linear Regression")
+    ax.set_zlabel("Custo", rotation=90)
+    ax.set_title("Custo do erro quadrático na regressão linear")
 
     plt.show()
 
 
 def plt_simple_example(x, y):
-    """ plots tumor data """
+    """plots tumor data"""
     pos = y == 1
     neg = y == 0
 
-    fig,ax = plt.subplots(1,1,figsize=(5,3))
+    fig, ax = plt.subplots(1, 1, figsize=(5, 3))
     fig.canvas.toolbar_visible = False
     fig.canvas.header_visible = False
     fig.canvas.footer_visible = False
 
-    ax.scatter(x[pos], y[pos], marker='x', s=80, c = 'red', label="malignant")
-    ax.scatter(x[neg], y[neg], marker='o', s=100, label="benign", facecolors='none', edgecolors=dlblue,lw=3)
-    ax.set_ylim(-0.075,1.1)
-    ax.set_ylabel('y')
-    ax.set_xlabel('Tumor Size')
-    ax.legend(loc='lower right')
-    ax.set_title("Example of Logistic Regression on Categorical Data")
+    ax.scatter(x[pos], y[pos], marker="x", s=80, c="red", label="malignant")
+    ax.scatter(
+        x[neg],
+        y[neg],
+        marker="o",
+        s=100,
+        label="benign",
+        facecolors="none",
+        edgecolors=dlblue,
+        lw=3,
+    )
+    ax.set_ylim(-0.075, 1.1)
+    ax.set_ylabel("y")
+    ax.set_xlabel("Tumor Size")
+    ax.legend(loc="lower right")
+    ax.set_title("Exemplo de regressão logística em dados categóricos")
 
 
 def plt_two_logistic_loss_curves():
-    """ plots the logistic loss """
-    fig,ax = plt.subplots(1,2,figsize=(6,3),sharey=True)
+    """plots the logistic loss"""
+    fig, ax = plt.subplots(1, 2, figsize=(6, 3), sharey=True)
     fig.canvas.toolbar_visible = False
     fig.canvas.header_visible = False
     fig.canvas.footer_visible = False
-    x = np.linspace(0.01,1-0.01,20)
-    ax[0].plot(x,-np.log(x))
-    #ax[0].set_title("y = 1")
+    x = np.linspace(0.01, 1 - 0.01, 20)
+    ax[0].plot(x, -np.log(x))
+    # ax[0].set_title("y = 1")
     ax[0].text(0.5, 4.0, "y = 1", fontsize=12)
     ax[0].set_ylabel("loss")
     ax[0].set_xlabel(r"$f_{w,b}(x)$")
-    ax[1].plot(x,-np.log(1-x))
-    #ax[1].set_title("y = 0")
+    ax[1].plot(x, -np.log(1 - x))
+    # ax[1].set_title("y = 0")
     ax[1].text(0.5, 4.0, "y = 0", fontsize=12)
     ax[1].set_xlabel(r"$f_{w,b}(x)$")
-    ax[0].annotate("prediction \nmatches \ntarget ", xy= [1,0], xycoords='data',
-                 xytext=[-10,30],textcoords='offset points', ha="right", va="center",
-                   arrowprops={'arrowstyle': '->', 'color': dlorange, 'lw': 3},)
-    ax[0].annotate("loss increases as prediction\n differs from target", xy= [0.1,-np.log(0.1)], xycoords='data',
-                 xytext=[10,30],textcoords='offset points', ha="left", va="center",
-                   arrowprops={'arrowstyle': '->', 'color': dlorange, 'lw': 3},)
-    ax[1].annotate("prediction \nmatches \ntarget ", xy= [0,0], xycoords='data',
-                 xytext=[10,30],textcoords='offset points', ha="left", va="center",
-                   arrowprops={'arrowstyle': '->', 'color': dlorange, 'lw': 3},)
-    ax[1].annotate("loss increases as prediction\n differs from target", xy= [0.9,-np.log(1-0.9)], xycoords='data',
-                 xytext=[-10,30],textcoords='offset points', ha="right", va="center",
-                   arrowprops={'arrowstyle': '->', 'color': dlorange, 'lw': 3},)
-    plt.suptitle("Loss Curves for Two Categorical Target Values", fontsize=12)
+    ax[0].annotate(
+        "predição \nmatches \nalvo ",
+        xy=[1, 0],
+        xycoords="data",
+        xytext=[-10, 30],
+        textcoords="offset points",
+        ha="right",
+        va="center",
+        arrowprops={"arrowstyle": "->", "color": dlorange, "lw": 3},
+    )
+    ax[0].annotate(
+        "aperda quando que a previsão\n difere da meta",
+        xy=[0.1, -np.log(0.1)],
+        xycoords="data",
+        xytext=[10, 30],
+        textcoords="offset points",
+        ha="left",
+        va="center",
+        arrowprops={"arrowstyle": "->", "color": dlorange, "lw": 3},
+    )
+    ax[1].annotate(
+        "predição \nmatches \nalvo ",
+        xy=[0, 0],
+        xycoords="data",
+        xytext=[10, 30],
+        textcoords="offset points",
+        ha="left",
+        va="center",
+        arrowprops={"arrowstyle": "->", "color": dlorange, "lw": 3},
+    )
+    ax[1].annotate(
+        "a perda aumenta quando a previsão\n difere da meta",
+        xy=[0.9, -np.log(1 - 0.9)],
+        xycoords="data",
+        xytext=[-10, 30],
+        textcoords="offset points",
+        ha="right",
+        va="center",
+        arrowprops={"arrowstyle": "->", "color": dlorange, "lw": 3},
+    )
+    plt.suptitle("Curvas de perda para dois valores-alvo categóricos", fontsize=12)
     plt.tight_layout()
     plt.show()
